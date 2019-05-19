604deb8443112550836a095829b6c65f1dd7418e
yaslyn <1174718834@qq.com>
2019-05-19 15:52:34 +0800

    更新app测试用例的模版

--- funcion/lottery.py
+++ funcion/lottery.py
@@ -1,39 +1,39 @@
 # 随机生成双色球，大乐透
 import random
-l=[]
-m=[]
+ssqred=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33]
+ssqblue=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+dltred=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35]
+daltblue=[1,2,3,4,5,6,7,8,9,10,11,12]
 # 双色球
 def shuangseqiu():
-
     # 生成6个红球
-    for i in range(6):
-        l.append(random.randint(1,33))
+    l=random.sample(ssqred,6)
     # 生成1个篮球
-    for k in range(1):
-        m.append(random.randint(1,16))
+    m=random.sample(ssqblue,1)
     l.sort()
-    print(l,m)
+    print("随机双色球红球是:",l,"蓝球是:",m)
 # 大乐透
 def daletou():
     # 生成5个红球
-    for i in range(5):
-        l.append(random.randint(1, 35))
-    # 生成1个篮球
-    for k in range(2):
-        m.append(random.randint(1, 12))
+    l=random.sample(dltred,5)
+    # 生成2个篮球
+    m=random.sample(daltblue,2)
     l.sort()
     m.sort()
-    print(l, m)
+    print("随机大乐透红球是:", l, "蓝球是:", m)
 def zdyssq():
     l=random.sample([12,17,9,4,18,3,10,2,1,8,20,19,28,32],6)
-    m=random.sample([12,17,9,4,18,3,10,2,1,8,20,19,28,32],1)
-    print("双色球红球是：",l,"蓝球是：",m)
+    l.sort()
+    m=random.sample([12,17,9,4,3,10,2,1,8],1)
+    print("指定双色球红球是：",l,"蓝球是：",m)
 def zdydlt():
     l = random.sample([12, 17, 9, 4, 18, 3, 10, 2, 1, 8, 20, 19, 28, 32], 5)
     l.sort()
-    m = random.sample([12, 17, 9, 4, 18, 3, 10, 2, 1, 8, 20, 19, 28, 32], 2)
+    m = random.sample([12, 9, 4, 3, 10, 2, 1, 8], 2)
     m.sort()
-    print("大乐透红球是：", l, "蓝球是：", m)
+    print("指定大乐透红球是：", l, "蓝球是：", m)
 
 zdydlt()
 zdyssq()
+shuangseqiu()
+daletou()
