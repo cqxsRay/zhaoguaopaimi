ba50b29b0d8a8a744311a5b68f47903253658ac0
yaslyn <1174718834@qq.com>
2019-05-19 14:38:29 +0800

    更新app测试用例的模版

---
+++ tt.py
@@ -0,0 +1,4 @@
+a={'content':str({'confirmPassword':'111111','loginName':'yuan',
+                      'loginPassword':'111111','mobile':'14711234500','smsAuthType':1,
+                    'smsAuthCode':'111111','userType':1})}
+print(a)
